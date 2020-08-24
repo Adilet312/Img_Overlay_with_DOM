c14a5670edc3c0eaa93c8e338387f033d46314e1
Adilet312 <adiletm@hotmail.com>
2020-08-24 15:00:14 -0700

    Add close option.

--- src/dom_img.js
+++ src/dom_img.js
@@ -1,15 +1,42 @@
 import './css/styles.css';
 import coffee from './assets/coffee.jpg';
 import jeans from './assets/main.jpeg';
- function displayImg(){
-  let image = document.createElement('img');
-      image.setAttribute('src',`${coffee}`);
-      image.className = "imgClass";
-      document.body.appendChild(image);
+import jeans1 from './assets/jeans1.jpeg';
+import jeans2 from './assets/jeans2.jpeg';
+import jeans3 from './assets/jeans3.jpeg';
+import jeans4 from './assets/jeans4.jpeg';
+function displayImg(){
+  let pictures = document.querySelector('ul').children;
+  for(let idx = 0; idx <pictures.length; idx++){
+    let img = document.createElement('img');
+    if(idx===0){
+        img.setAttribute('src',`${jeans1}`);
+        img.id=`${idx}`
+        pictures[idx].appendChild(img);
+    }
+    else if(idx ===1){
+      img.setAttribute('src',`${jeans2}`);
+      img.id=`${idx}`
+      pictures[idx].appendChild(img);
+    }
+    else if(idx===2){
+      img.setAttribute('src',`${jeans3}`);
+      img.id=`${idx}`
+      pictures[idx].appendChild(img);
+    }
+    else{
+      img.setAttribute('src',`${jeans4}`);
+      img.id=`${idx}`
+      pictures[idx].appendChild(img);
+    }
+
+  }
+
+
 }
 displayImg();
 
-function displayOverlay(){
+function displayOverlay(givenId){
 
     /*Overlay*/
     let overlay =  document.createElement('div');
@@ -29,30 +56,50 @@ function displayOverlay(){
 
     /*Figure and Large Img*/
     let figure = document.createElement('figure');
-    let enlargeImg = document.createElement('img');
-    enlargeImg.className = "bigImgClass";
-    enlargeImg.src = `${jeans}`;
-    figure.appendChild(enlargeImg);
+    let img = document.createElement('img');
+    img.className = "bigImgClass";
+    if(givenId===0){
+        img.setAttribute('src',`${jeans1}`);
+
+    }
+    else if(givenId ===1){
+      img.setAttribute('src',`${jeans2}`);
+    }
+    else if(givenId===2){
+      img.setAttribute('src',`${jeans3}`);
+    }
+    else {
+      img.setAttribute('src',`${jeans4}`);
+    }
+    figure.appendChild(img);
     document.body.appendChild(figure);
 
     /*Original Img to remove*/
-    let originalImg = document.querySelector('.imgClass');
+    let originalImg = document.querySelector('.pictures');
     originalImg.parentNode.removeChild(originalImg);
-
     /*Remove overlay when it is clicked*/
     document.querySelector('.overlayClass').addEventListener('click',function(e){
-      let overlay_element = e.target.parentNode;
+      let overlay_element = e.target;
+      let bigPicture = overlay_element.nextElementSibling;
       if(overlay_element){
         overlay_element.parentNode.removeChild(overlay_element);
+        bigPicture.parentNode.removeChild(bigPicture);
       }
     },false);
     document.querySelector('.close').addEventListener('click',function(e){
-      let overlay_element = e.target.parentNode;
-      if(overlay_element){
-        overlay_element.parentNode.removeChild(overlay_element);
-      }
+      let elements = e.target.parentNode.children;
+      let span = elements[1];
+      let overlay = elements[2];
+      let figure = elements[3];
+      span.parentNode.removeChild(span);
+      overlay.parentNode.removeChild(overlay);
+      figure.parentNode.removeChild(figure);
+      
     },false);
 
 
 }
-document.querySelector('.imgClass').addEventListener('click',displayOverlay,false);
+document.querySelector('.pictures').addEventListener('click',function(e){
+  console.log(e.target.id)
+  displayOverlay(Number(e.target.id));
+},false);
